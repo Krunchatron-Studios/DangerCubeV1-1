824649e4374a3070856d06055472861ebdb47e8c
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 14:02:22 -0400

    Test Push - Added Basic Enemy Stats

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour
 {
+	[Header("Enemy Stats")]
+	[SerializeField] private int _health;
+	[SerializeField] private int _speed;
+	private Rigidbody2D _enemyRB;
 	
 }
