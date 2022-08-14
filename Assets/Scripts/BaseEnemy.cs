335fd01aa70ad54260e6d6a8b7e547a214a5f825
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-08-14 16:29:13 -0500

    refactoring of BaseEnemy script

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
-
 public class BaseEnemy : MonoBehaviour
 {
+	[Header("Enemy Stats")]
+	public int healthMax;
+	public int currentHealth;
+	public int damage;
+	public int moveSpeed;
 	
+	[Header("Movement Dependencies")]
+	public Rigidbody2D enemyRb2D;
+	public Transform playerPosition;
+	void Start()
+	{
+		playerPosition = GameObject.FindWithTag("Player").transform;
+	}
+	void FixedUpdate()
+	{
+		MoveTowardsPlayer();
+	}
+
+	private void MoveTowardsPlayer()
+	{
+		Vector3 temp = Vector3.MoveTowards(transform.position, playerPosition.position, moveSpeed * Time.deltaTime);
+		enemyRb2D.MovePosition(temp);
+	}
 }
