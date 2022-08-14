de4721458af9084c1a0ece3a7c6d52006daa0307
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 19:06:53 -0400

    8/14 7:07pm Pre-Merge Commit

--- Assets/Scripts/Testing/DealDamage.cs
+++ Assets/Scripts/Testing/DealDamage.cs
@@ -16,7 +16,7 @@ public class DealDamage : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         int damage = collision.gameObject.GetComponent<BaseEnemy>()._damage;
-        Debug.Log("this is how much damage is being pased: " + damage);
+        Debug.Log("this is how much damage is being passed: " + damage);
         if (collision.CompareTag("Enemy"))
         {
             StartCoroutine(DamageCo(damage));
@@ -27,6 +27,6 @@ public class DealDamage : MonoBehaviour
         _currentHealth = _currentHealth - damage;
         yield return null;
         yield return new WaitForSeconds(1f);
-        Debug.Log(_currentHealth);
+        Debug.Log("This is the current health of the test object: " + _currentHealth);
     }
 }
