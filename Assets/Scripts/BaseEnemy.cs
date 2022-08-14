23dd07b1895806672080133836aa799532518d95
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 14:21:03 -0400

    8/14 2:20pm - Adding Enemy Chase Logic - Test Enemy Sub-Class Inherits From BaseEnemy Class

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -5,6 +5,20 @@ public class BaseEnemy : MonoBehaviour
 	[Header("Enemy Stats")]
 	[SerializeField] private int _health;
 	[SerializeField] private int _speed;
-	private Rigidbody2D _enemyRB;
-	
+	public Transform _playerPosition;
+
+	void Start()
+	{
+		_playerPosition = GameObject.FindWithTag("Player").transform;
+	}
+
+	void FixedUpdate()
+	{
+		MoveTowardsPlayer();
+	}
+
+	private void MoveTowardsPlayer()
+	{
+		Vector3 temp = Vector3.MoveTowards(transform.position, _playerPosition.position, _speed * Time.deltaTime);
+	}
 }
