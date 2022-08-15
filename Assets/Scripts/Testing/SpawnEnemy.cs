13b74b5c5a8f5075dc8d7b052f232bea5186c4a0
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 20:53:10 -0400

    8/14 8:52pm Enemy Spawner Randomizing Well

--- Assets/Scripts/Testing/SpawnEnemy.cs
+++ Assets/Scripts/Testing/SpawnEnemy.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    [Header("Spawn Parameters")]
     [SerializeField] private float _waitTime;
     private float _nextSpawn;
     [SerializeField] private int _spawnQuantity;
+    [Header("Game Objects")]
     [SerializeField] private GameObject _enemy;
     [SerializeField] private Transform _playerPosition;
     void Update()
@@ -23,9 +25,41 @@ public class SpawnEnemy : MonoBehaviour
     private void Spawn()
     {
         _playerPosition = GameObject.FindWithTag("Player").transform;
-        float tempX = _playerPosition.position.x + Random.Range(10.0f, 20.0f);
-        float tempY = _playerPosition.position.y + Random.Range(10.0f, 20.0f);
-        Debug.Log(tempX + "/" + tempY);
+        float tempX = _playerPosition.position.x;
+        float tempY = _playerPosition.position.y;
+        int randomizer = Random.Range(1, 9);
+        switch (randomizer)
+        {
+            case 1:
+                tempX = _playerPosition.position.x + Random.Range(10.0f, 20.0f);
+                tempY = _playerPosition.position.y + Random.Range(10.0f, 20.0f);
+                break;
+            case 2:
+                tempX = _playerPosition.position.x + Random.Range(-10.0f, -20.0f);
+                tempY = _playerPosition.position.y + Random.Range(-10.0f, -20.0f);
+                break;
+            case 3:
+                tempX = _playerPosition.position.x + Random.Range(-10.0f, -20.0f);
+                tempY = _playerPosition.position.y + Random.Range(10.0f, 20.0f);
+                break;
+            case 4:
+                tempX = _playerPosition.position.x + Random.Range(10.0f, 20.0f);
+                tempY = _playerPosition.position.y + Random.Range(-10.0f, -20.0f);
+                break;
+            case 5:
+                tempX = _playerPosition.position.x + Random.Range(10.0f, 20.0f);
+                break;
+            case 6:
+                tempX = _playerPosition.position.x + Random.Range(-10.0f, -20.0f);
+                break;
+            case 7:
+                tempY = _playerPosition.position.y + Random.Range(-10.0f, -20.0f);
+                break;
+            case 8:
+                tempY = _playerPosition.position.y + Random.Range(10.0f, 20.0f);
+                break;
+        }
+        
         Instantiate(_enemy, new Vector3(tempX, tempY, 0), Quaternion.identity);
     }
 }
