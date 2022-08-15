0d8170332321d0b4f7d27486191e20076d4ad76b
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-15 11:07:09 -0400

    8/15 11:06 Reformated to K&R Style - Updated Naming Conventions - Deleted Redundant Folder

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class BaseEnemy : MonoBehaviour, IDmgAndHpInterface
-{
+public class BaseEnemy : MonoBehaviour, IDmgAndHpInterface {
 	[Header("Enemy Stats")] public int maxHealth = 10;
 	public int currentHealth = 10;
 	public int damage = 1;
@@ -10,33 +9,27 @@ public class BaseEnemy : MonoBehaviour, IDmgAndHpInterface
 	public Transform playerPosition;
 	[Header("Spawn Mechanics")] public float spawnDistance;
 
-	void Start()
-	{
+	void Start() {
 		playerPosition = GameObject.FindWithTag("Player").transform;
 	}
 
-	void FixedUpdate()
-	{
+	void FixedUpdate() {
 		MoveTowardsPlayer();
 	}
 
-	public virtual void MoveTowardsPlayer()
-	{
+	public virtual void MoveTowardsPlayer() {
 		Vector3 temp = Vector3.MoveTowards(transform.position, playerPosition.position, moveSpeed * Time.deltaTime);
 		enemyRb2D.MovePosition(temp);
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
+	void OnTriggerEnter2D(Collider2D other) {
 		IDmgAndHpInterface hit = other.GetComponent<IDmgAndHpInterface>();
-		if (other.CompareTag("Enemy"))
-		{
+		if (other.CompareTag("Enemy")) {
 			hit.TakeDamage(damage);
 		}
 	}
 	//
-	public void TakeDamage(int dmgAmount)
-	{
+	public void TakeDamage(int dmgAmount) {
 		currentHealth -= dmgAmount;
 		Debug.Log("Current health is: " + currentHealth);
 		// if (currentHealth <= 0)
@@ -48,8 +41,7 @@ public class BaseEnemy : MonoBehaviour, IDmgAndHpInterface
 	public void HealDamage(int healAmount) {
 		currentHealth += healAmount;
 		// play some animation of healing
-		if (currentHealth > maxHealth)
-		{
+		if (currentHealth > maxHealth) {
 			currentHealth = maxHealth;
 		}
 	}
