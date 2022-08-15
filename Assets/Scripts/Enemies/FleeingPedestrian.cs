cc771337f1b592431fa7f6ff2ae4f80cb40de1ec
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 21:19:27 -0400

    8/14 9:19pm Fleeing Pedestrian Now Disappear After A Given Time

--- Assets/Scripts/Enemies/FleeingPedestrian.cs
+++ Assets/Scripts/Enemies/FleeingPedestrian.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class FleeingPedestrian : BaseEnemy
 {
+    [SerializeField] private float _safetyTime;
+
+    void Start()
+    {
+        _playerPosition = GameObject.FindWithTag("Player").transform;
+        StartCoroutine(SuccessfullyFled());
+    }
     public override void MoveTowardsPlayer()
     {
         Vector3 temp = Vector3.MoveTowards(transform.position, _playerPosition.position, -1 * _speed * Time.deltaTime);
         _enemyRB.MovePosition(temp);
+    }
+
+    private IEnumerator SuccessfullyFled()
+    {
+        yield return null;
+        yield return new WaitForSeconds(_safetyTime);
+        Destroy(this.gameObject);
     }
 }
