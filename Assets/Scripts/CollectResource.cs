d7d1b8fb12d15c6353f6e23e99e367e48cafb017
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-16 06:49:27 -0400

    8/16 6:49pm Resource Collection Working Through Scriptable Objects

--- Assets/Scripts/CollectResource.cs
+++ Assets/Scripts/CollectResource.cs
@@ -3,43 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectResource : MonoBehaviour {
-    
-    public int bioGoo = 0;
-    public int metal = 0;
-    public int silicate = 0;
+
+    public int value = 2;
+    public PlayerResources PlayerResources;
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            switch (gameObject.name) {
-                case "SmallBioGoo(Clone)":
-                    Debug.Log(bioGoo);
-                    bioGoo += 1;
-                    Debug.Log("You have " + bioGoo + " small bio goo.");
-                    break;
-                case "MediumBioGoo(Clone)":
-                    bioGoo += 5;
-                    break;
-                case "LargeBioGoo(Clone)":
-                    bioGoo += 10;
-                    break;
-                case "SmallMetal(Clone)":
-                    metal++;
-                    break;
-                case "MediumMetal(Clone)":
-                    metal += 5;
-                    break;
-                case "LargeMetal(Clone)":
-                    metal += 10;
-                    break;
-                case "SmallSilicate(Clone)":
-                    silicate++;
-                    break;
-                case "MediumSilicate(Clone)":
-                    silicate += 5;
-                    break;
-                case "LargeSilicate(Clone)":
-                    silicate += 10;
-                    break;
-            }
+            PlayerResources.bioGoo += value;
+            Destroy(this.gameObject);
         }
+       
     }
 }
---
+++ Assets/Scripts/ScriptableCode/PlayerResources.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Resources", menuName = "ScriptableObjects/PlayerResources")]
+public class PlayerResources : ScriptableObject {
+    public int bioGoo;
+    public int metal;
+    public int silicate;
+}
