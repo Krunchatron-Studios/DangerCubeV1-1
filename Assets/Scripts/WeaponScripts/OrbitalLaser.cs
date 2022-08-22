0976828c046f562cda778cc0605e4f0650a44537
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-08-22 14:50:55 -0500

    adding changes from Tdev

--- Assets/Scripts/WeaponScripts/OrbitalLaser.cs
+++ Assets/Scripts/WeaponScripts/OrbitalLaser.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
 
 public class OrbitalLaser : Weapon {
     private Transform _playerPosition;
