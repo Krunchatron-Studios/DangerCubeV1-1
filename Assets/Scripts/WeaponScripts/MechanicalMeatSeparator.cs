129b7db0f1f1d9401a9480a3bad34e451f7b4849
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-23 07:19:39 -0400

    8/23 7:19am Meat Saw Anchored Correctly But Not Swinging Well

--- Assets/Scripts/WeaponScripts/MechanicalMeatSeparator.cs
+++ Assets/Scripts/WeaponScripts/MechanicalMeatSeparator.cs
@@ -2,7 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MechanicalMeatSeparator : Weapon
-{
+public class MechanicalMeatSeparator : MonoBehaviour {
+    // public Joint2D sawHardPoint;
 
+    private GameObject _enemyPosition;
+
+    public Rigidbody2D thisRigidBody;
+
+    public float sawVelocity;
+
+    public float damage;
+    void Start() {
+        _enemyPosition = GameObject.FindWithTag("Enemy");
+    }
+    void Update() {
+        _enemyPosition = GameObject.FindWithTag("Enemy");
+        MoveSaw();
+    }
+
+    void MoveSaw() {
+        Vector3 moveDirection = (_enemyPosition.transform.position - transform.position).normalized;
+        thisRigidBody.AddForce(moveDirection * sawVelocity * Time.deltaTime, ForceMode2D.Impulse);
+    }
 }
