e7b63925d90e1ab85f23ed54aa5952da2f2f43db
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-27 08:35:09 -0400

    8/27 8:34am Added Nano Bot Manager Scriptable Object

--- Assets/Scripts/WeaponScripts/MutagenicNanobots.cs
+++ Assets/Scripts/WeaponScripts/MutagenicNanobots.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class MutagenicNanobots : Weapon
-{  
-    public int maxNanoBots;
-    public int currentNanoBots;
+public class MutagenicNanobots : Weapon {
+    public NanoManager nanoManager;
     public GameObject playerPosition;
     public Transform center;
     public GameObject nanoBot;
     public List<GameObject> allNanos = new List<GameObject>();
     public float rotationSpeed;
 
+    void Start() {
+        nanoManager.currentNanoBots = 0;
+    }
     void FixedUpdate() {
         playerPosition = GameObject.FindWithTag("Player");
         center = playerPosition.transform;
@@ -21,11 +22,11 @@ public class MutagenicNanobots : Weapon
     }
 
     private void SpawnNanos() {
-        for (int i = currentNanoBots; i <= maxNanoBots; i++) {
+        for (int i = nanoManager.currentNanoBots; i <= nanoManager.maxNanoBots; i++) {
             Vector3 offset = new Vector3(center.position.x, center.position.y + 3, 0);
             GameObject thisNano = Instantiate(nanoBot, offset, Quaternion.identity);
             allNanos.Add(thisNano);
-            currentNanoBots++;
+            nanoManager.currentNanoBots++;
         }
     }
 
@@ -33,6 +34,7 @@ public class MutagenicNanobots : Weapon
         foreach (GameObject thisNano in allNanos) {
             Vector3 direction = new Vector3(0, 1, 0);
             thisNano.transform.RotateAround(center.position, direction, rotationSpeed * Time.deltaTime);
+            // thisNano.transform.position = Vector3.MoveTowards(transform.position, center.position, rotationSpeed);
         }
     }
 }
--- Assets/Scripts/WeaponScripts/NanoBot.cs
+++ Assets/Scripts/WeaponScripts/NanoBot.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NanoBot : Projectile {
-
+    public NanoManager nanoManager;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Enemy")) {
             Debug.Log("This is a pedestrian impact: " + other.gameObject.name);
+            Destroy(this.gameObject);
+            nanoManager.currentNanoBots--;
         }
     }
 }
