36818eeed704aceb7521fe4aa720916fc8b67832
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-27 08:21:21 -0400

    8/27 8:21am Rotation Sort of Working But Not On The Correct Plane

--- Assets/Scripts/WeaponScripts/MutagenicNanobots.cs
+++ Assets/Scripts/WeaponScripts/MutagenicNanobots.cs
@@ -10,25 +10,29 @@ public class MutagenicNanobots : Weapon
     public GameObject playerPosition;
     public Transform center;
     public GameObject nanoBot;
+    public List<GameObject> allNanos = new List<GameObject>();
     public float rotationSpeed;
 
     void FixedUpdate() {
         playerPosition = GameObject.FindWithTag("Player");
         center = playerPosition.transform;
         SpawnNanos();
+        RotateNanos();
     }
 
     private void SpawnNanos() {
-        if (currentNanoBots < maxNanoBots) {
+        for (int i = currentNanoBots; i <= maxNanoBots; i++) {
             Vector3 offset = new Vector3(center.position.x, center.position.y + 3, 0);
             GameObject thisNano = Instantiate(nanoBot, offset, Quaternion.identity);
+            allNanos.Add(thisNano);
             currentNanoBots++;
-            RotateNanos(thisNano);
-        } 
+        }
     }
 
-    private void RotateNanos(GameObject thisNano) {
-        thisNano.transform.RotateAround(center.position, Vector3.left, rotationSpeed * Time.deltaTime);
-       
+    private void RotateNanos() {
+        foreach (GameObject thisNano in allNanos) {
+            Vector3 direction = new Vector3(0, 1, 0);
+            thisNano.transform.RotateAround(center.position, direction, rotationSpeed * Time.deltaTime);
+        }
     }
 }
