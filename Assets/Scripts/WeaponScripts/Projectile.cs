9608b52a5ef389c1082277894f60731296ded56d
Joshua T Edwards <51751072+Kptkrunch@users.noreply.github.com>
2022-08-27 05:26:42 -0500

    Jdev (#41)
    * added enemy flicker
    
    * adding unversioned files
    
    * minor changes
    
    * added shader for dissolve.  Still WIP
    
    * final merge commit
    
    * Added dissolve shader and dissolve script
    
    * Reinstalled "Feel"
    
    * Deathray visuals improved, flicker and dissolve work separately until color field is worked out
    
    * added dissolve effect to all enemies
    
    * small corrections to get things working without MMF Player flicker
    
    * added lights to main scene, cluster, and rapid projectiles
    
    * laser now dissolving and leaving ash in the wind
    
    * Lighting adjustments, 30 enemy spawners tested during runtime
    
    * Several adds
    
    Co-authored-by: Joshua T Edwards <Kptkrunch@users.noreply.github.com>

--- Assets/Scripts/WeaponScripts/Projectile.cs
+++ Assets/Scripts/WeaponScripts/Projectile.cs
@@ -8,37 +8,28 @@ public class Projectile : MonoBehaviour {
 	public Rigidbody2D projectileRb2D;
 	public Vector3 targetPosition;
 	public GameObject bloodSplash;
-	private float _distance;
-	
-	private void Update() {
-		_distance = Vector3.Distance(weapon.transform.position, transform.position);
-		if (_distance > weapon.weaponRange) {
-			Destroy(gameObject);
-		}
-	}
+	public Vector3 direction;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		ResolveProjectile(other);
-		if (other.CompareTag("Enemy")) {
-			MMFloatingTextSpawnEvent.Trigger(0, other.attachedRigidbody.transform.position, 
-				weapon.weaponDamage.ToString(), Vector3.up, .2f);
-			bloodSplash = Instantiate(bloodSplash, other.transform.position, Quaternion.identity);
-		}
 	}
-
-	
-
 	public void Setup(Vector3 targetPos) {
 		targetPosition = targetPos;
 	}
 	public void MoveProjectile() {
 		Vector3 moveDirection = (targetPosition - transform.position).normalized;
+		direction = moveDirection;
 		projectileRb2D.AddForce(moveDirection * projectileVelocity * Time.deltaTime, ForceMode2D.Impulse);
 	}
 	public virtual void ResolveProjectile(Collider2D other) {
 		if (other.CompareTag("Enemy")) {
+			Debug.Log("Hello");
 			IDmgAndHpInterface hit = other.GetComponent<IDmgAndHpInterface>();
 			hit.TakeDamage(weapon.weaponDamage);
+			MMFloatingTextSpawnEvent.Trigger(0, other.attachedRigidbody.transform.position, 
+				weapon.weaponDamage.ToString(), Vector3.up, .2f);
+			weapon.enemyKnockBack(direction, other.attachedRigidbody);
+			bloodSplash = Instantiate(bloodSplash, other.transform.position, Quaternion.identity);
 			Destroy(gameObject);
 		}
 		if (other.CompareTag("Wall")) {
