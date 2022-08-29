30f349188d43dd87dd32c5b5727a6619b9328b06
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-08-28 22:30:58 -0500

    started working on the shield bar

--- Assets/Scripts/EnemyScripts/Kiwikid.cs
+++ Assets/Scripts/EnemyScripts/Kiwikid.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kiwikid : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Kiwikid : BaseEnemy {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	
 }
