0658f7197a80015ce991faa3c766cc52fbc10993
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-31 15:54:46 -0400

    8/31 3:54pm Working On Screen Fades

--- Assets/Scripts/IngameUI/LevelSelect.cs
+++ Assets/Scripts/IngameUI/LevelSelect.cs
@@ -1,13 +1,30 @@
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
-public class LevelSelect : MonoBehaviour
-{
+public class LevelSelect : MonoBehaviour {
+
+    public GameObject fadeIn;
+    public GameObject fadeOut;
+
+    void Start() {
+        fadeIn.SetActive(true);
+        StartCoroutine(Toggle(fadeOut));
+    }
+
     public void GoToEarth() {
+        fadeIn.SetActive(true);
+        StartCoroutine(Toggle(fadeIn));
         SceneManager.LoadScene("TestLevel");
+    }
+
+    private IEnumerator Toggle(GameObject fade) {
+        yield return new WaitForSeconds(1);
+        fade.SetActive(false);
     }
 }
