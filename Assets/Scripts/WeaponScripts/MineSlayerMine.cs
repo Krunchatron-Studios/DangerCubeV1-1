2efdabef3ac8bbdf0328dbf1ca434333102b2773
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-09-04 22:01:18 -0500

    Mine slayer working as intended and completed

--- Assets/Scripts/WeaponScripts/MineSlayerMine.cs
+++ Assets/Scripts/WeaponScripts/MineSlayerMine.cs
@@ -3,33 +3,35 @@ using UnityEngine;
 
 public class MineSlayerMine : ParticleProjectile {
 
-	[Header("AcidMinePool goes here")]
-	public MMSimpleObjectPooler minePooler;
-	public MMSimpleObjectPooler blastPooler;
-	public CircleCollider2D blastCircle;
-	private float blastRadius;
+	[SerializeField] private float blastRadius = 3f;
 	
-	private void OnTriggerStay2D(Collider2D other) {
-		blastRadius = weapon.weaponRange;
+	private void OnTriggerEnter2D(Collider2D other) {
 		if (other.CompareTag("Enemy")) {
-			blastCircle.radius = weapon.weaponRange;
 			ResolveProjectile(other);
 		}
 	}
 	
 	public override void ResolveProjectile(Collider2D other) {
 		GameObject blast = PoolManager.pm.acidBlastPool.GetPooledGameObject();
-		blast.gameObject.SetActive(true);
-		blast.transform.position = transform.position;
+		blast.SetActive(true);
+		blast.transform.position = other.transform.position;
+		HitBlast();
+		gameObject.SetActive(false);
+	}
+
+	private void HitBlast() {
 		Collider2D[] colliderArray = Physics2D.OverlapCircleAll(transform.position, blastRadius);
 		foreach(Collider2D col in colliderArray) {
-			if (other.CompareTag("Enemy")) {
+			if (col.CompareTag("Enemy")) {
 				IDmgAndHpInterface hit = col.GetComponent<IDmgAndHpInterface>();
 				hit.TakeDamage(weapon.weaponDamage, weapon.damageType);
+				Debug.Log("this is a test3");
+
 				MMFloatingTextSpawnEvent.Trigger(0, col.attachedRigidbody.transform.position, 
 					weapon.weaponDamage.ToString(), Vector3.up, .2f);
+				Debug.Log("this is a test4");
+
 			}
 		}
-		gameObject.SetActive(false);
 	}
 }
