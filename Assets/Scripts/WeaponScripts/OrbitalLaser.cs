cedcac968cffaec478406c713d55f003cd20647f
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-09-23 08:42:45 -0400

    9/23 8:42am Refactoring Orbital for Object Pooling

--- Assets/Scripts/WeaponScripts/OrbitalLaser.cs
+++ Assets/Scripts/WeaponScripts/OrbitalLaser.cs
@@ -4,48 +4,45 @@ using UnityEngine;
 public class OrbitalLaser : ProjectileWeapon {
     private Transform _playerPosition;
     private Vector3 _enemyPosition;
-    public GameObject explosion;
+    public Projectile explosion;
     private void FixedUpdate() {
-        CanFireTimer();
         _playerPosition = GameObject.FindWithTag("Player").transform;
-        _enemyPosition = targetingSys.weapon.GetComponent<ProjectileWeapon>().enemyTarget;
-        // if (GameObject.FindWithTag("Enemy")) {
-        //     _enemyPosition = GameObject.FindWithTag("Enemy").transform.position;
-        //     Debug.Log($"enemy position: {_enemyPosition}");
-        // }
     }
     public override void FireWeapon(Vector3 firePoint, Vector3 targetPosition) {
-        GameObject bulletTransform = Instantiate(projectile, _enemyPosition, Quaternion.Euler(0,0,90));
-        StartCoroutine(BeamCo(bulletTransform));
-        Projectile bullet = bulletTransform.GetComponent<Projectile>();
+        // GameObject bulletTransform = Instantiate(projectile, _enemyPosition, Quaternion.Euler(0,0,90));
+        GameObject spawnedBullet = objectPooler.GetPooledGameObject();
+        Projectile bullet = spawnedBullet.GetComponent<Projectile>();
+        StartCoroutine(BeamCo(bullet));
         nextFire = Time.time + attackSpeed;
     }
 
-    IEnumerator BeamCo(GameObject bulletTransform) {
-        float distance = Vector3.Distance(bulletTransform.transform.position, _enemyPosition);
-        GameObject explosionTransform = Instantiate(explosion, _enemyPosition, Quaternion.identity);
-        StartCoroutine(ExplodeCo(explosionTransform));
+    IEnumerator BeamCo(Projectile bullet) {
+        float distance = Vector3.Distance(bullet.transform.position, _enemyPosition);
+        // GameObject explosionTransform = Instantiate(explosion, _enemyPosition, Quaternion.identity);
+        GameObject explosion = objectPooler.GetPooledGameObject();
+        Projectile explosion = this.explosion.GetComponent<Projectile>();
+        StartCoroutine(ExplodeCo(explosion));
         audioSource.Play();
         float time = 0;
         while (time < 1) {
-            bulletTransform.transform.localScale = new Vector3(Mathf.Lerp(2, 0, time / 1), distance + 5, 0);
+            bullet.transform.localScale = new Vector3(Mathf.Lerp(2, 0, time / 1), distance + 5, 0);
             time += Time.deltaTime;
             yield return null;
         }
-        Destroy(bulletTransform.gameObject);
+        bullet.gameObject.SetActive(false);
     }
 
-    IEnumerator ExplodeCo(GameObject laserSwirl) {
-        var localScale = laserSwirl.transform.localScale;
+    IEnumerator ExplodeCo(Projectile explosion) {
+        var localScale = explosion.transform.localScale;
         float currentY = localScale.y;
         float currentX = localScale.x;
         int lerpMax = 2;
         float time = 0;
-        while (laserSwirl.transform.localScale.x > 0.1f) {
-            laserSwirl.transform.localScale = new Vector3(Mathf.Lerp(currentX, 0, time / lerpMax), Mathf.Lerp(currentY, 0, time / lerpMax), 0);
+        while (explosion.transform.localScale.x > 0.1f) {
+            explosion.transform.localScale = new Vector3(Mathf.Lerp(currentX, 0, time / lerpMax), Mathf.Lerp(currentY, 0, time / lerpMax), 0);
             time += Time.deltaTime;
             yield return null;
         } 
-        Destroy(laserSwirl);
+        explosion.gameObject.SetActive(false);
     }
 }
