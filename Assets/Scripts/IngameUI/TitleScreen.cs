70ed7cfd21ae2bb3f7283a34f68fbd86b7f67ac0
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-09-26 12:33:23 -0500

    added button sounds to title screen button presses

--- Assets/Scripts/IngameUI/TitleScreen.cs
+++ Assets/Scripts/IngameUI/TitleScreen.cs
@@ -4,18 +4,25 @@ using UnityEngine.SceneManagement;
 public class TitleScreen : MonoBehaviour {
 
 	public AudioSource audioSource;
-	public void StartGame() {
+
+	private void Start() {
 		audioSource.Play();
+	}
+
+	public void StartGame() {
+		audioSource.Stop();
+		SoundManager.sm.buttonPress.Play();
 		SceneManager.LoadScene("TestLevel");
 	}
 
 	public void LoadLevel(string lvlToLoad) {
-		SoundManager.sm.buttonPress.Play();
 		audioSource.Stop();
+		SoundManager.sm.buttonPress.Play();
 		SceneManager.LoadScene(lvlToLoad);
 	}
 
 	public void ExitToDesktop() {
+		audioSource.Stop();
 		SoundManager.sm.buttonPress.Play();
 		Application.Quit();
 	}
