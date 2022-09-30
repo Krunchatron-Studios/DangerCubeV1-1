8c786bffb39fbf9606e36f43cf59203abb924856
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-09-29 22:31:27 -0500

    updated aggro script to be dynamic

--- Assets/Scripts/AI/DistanceDetect.cs
+++ Assets/Scripts/AI/DistanceDetect.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class DistanceDetect : MonoBehaviour {
 
+    public float aggroRadius = 10.0f;
     public BaseEnemy[] enemyArray;
     void Update() {
         ScanForAggro();
@@ -13,7 +14,7 @@ public class DistanceDetect : MonoBehaviour {
         for (int i = 0; i < enemyArray.Length; i++) {
             BaseEnemy currentEnemy = enemyArray[i];
             float distance = Vector3.Distance(currentEnemy.transform.position, position);
-            if (distance < 10.0f) {
+            if (distance < aggroRadius) {
                 currentEnemy.GetComponent<AIMoveTorward>().hasEngaged = true;
             }
         }
