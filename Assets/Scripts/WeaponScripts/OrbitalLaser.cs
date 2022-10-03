62bd55ac905b3f4fa280c5d837c27799afee86da
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-10-03 11:11:16 -0400

    10/3 11:10am Debugged Issues With Laser Swirl - Pushing Changes Before Upgrades

--- Assets/Scripts/WeaponScripts/OrbitalLaser.cs
+++ Assets/Scripts/WeaponScripts/OrbitalLaser.cs
@@ -1,9 +1,10 @@
 using System.Collections;
+using Managers;
 using UnityEngine;
 
 public class OrbitalLaser : ProjectileWeapon {
     private Transform _playerPosition;
-    public GameObject explosion;
+    // public GameObject explosion;
     
     private void Update() {
         CanFireTimer();
@@ -21,7 +22,6 @@ public class OrbitalLaser : ProjectileWeapon {
         }
     }
     public override void FireWeapon(Vector3 firePoint, Vector3 targetPosition) {
-        Debug.Log(targetPosition);
         GameObject spawnedBullet = objectPooler.GetPooledGameObject();
         Projectile bullet = spawnedBullet.GetComponent<Projectile>();
         bullet.gameObject.SetActive(true);
@@ -36,8 +36,10 @@ public class OrbitalLaser : ProjectileWeapon {
         
         GameObject spawnedExplosion = PoolManager.pm.laserSwirlPool.GetPooledGameObject();
         Projectile explosion = spawnedExplosion.GetComponent<Projectile>();
+        
         explosion.transform.localScale = new Vector3(1, 1, 1);
         explosion.gameObject.SetActive(true);
+        
         explosion.transform.position = enemyTarget;
         StartCoroutine(ExplodeCo(explosion));
         
@@ -53,16 +55,20 @@ public class OrbitalLaser : ProjectileWeapon {
     }
 
     IEnumerator ExplodeCo(Projectile explosion) {
+
         var localScale = explosion.transform.localScale;
-        float currentY = localScale.y;
-        float currentX = localScale.x;
-        int lerpMax = 2;
         float time = 0;
         while (explosion.transform.localScale.x > 0.1f) {
-            explosion.transform.localScale = new Vector3(Mathf.Lerp(currentX, 0, time / lerpMax), Mathf.Lerp(currentY, 0, time / lerpMax), 0);
+            explosion.transform.localScale = new Vector3(Mathf.Lerp(localScale.x, 0, time / 2), Mathf.Lerp(localScale.y, 0, time / 2), 0);
             time += Time.deltaTime;
             yield return null;
         } 
         explosion.gameObject.SetActive(false);
     }
+
+    public void testFunc() {
+        FireWeapon(new Vector3(0, 0, 0), new Vector3(0, 0 , 0));
+    }
 }
+
+
