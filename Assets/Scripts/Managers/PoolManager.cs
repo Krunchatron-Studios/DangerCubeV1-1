6dc8d6612d9310e5761efe65d18836508075442e
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-10-06 00:10:25 -0500

    added muzzleflash and projectile impact pools for all weapons and enemies, added ultimate shader

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -15,6 +15,20 @@ namespace Managers {
 		public MMSimpleObjectPooler acidBlastPool;
 		public MMSimpleObjectPooler laserSwirlPool;
 
+		[Header("Muzzle Flashes")] 
+		public MMSimpleObjectPooler plasmaBursterMuzzle;
+		public MMSimpleObjectPooler clusterCannonMuzzle;
+		public MMSimpleObjectPooler mineSlayerMuzzle;
+		public MMSimpleObjectPooler orbitalLaserMuzzle;
+		public MMSimpleObjectPooler nanoBotsMuzzle;
+		
+		[Header("Impact Particles")]
+		public MMSimpleObjectPooler plasmaBursterImpact;
+		public MMSimpleObjectPooler clusterCannonImpact;
+		public MMSimpleObjectPooler mineSlayerImpact;
+		public MMSimpleObjectPooler orbitalLaserImpact;
+		public MMSimpleObjectPooler nanoBotsImpact;
+		
 		[Header("Resource Pools")] 
 		public MMSimpleObjectPooler sBioGooPool;
 		public MMSimpleObjectPooler mBioGooPool;
